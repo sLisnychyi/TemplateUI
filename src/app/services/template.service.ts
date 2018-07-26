22c6eb1937d4bf15c49e6b0eb093c8327eeba62c
sLisnychyi <lisnychyis@gmail.com>
2018-07-26 13:59:37 +0300

    added adittional fields

--- src/app/services/template.service.ts
+++ src/app/services/template.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs/index';
+import {Observable} from 'rxjs';
 import {Template} from '../models/template.model';
 import {TemplateRequest} from '../models/templateRequest';
 
